0796c9286679e29c1a3353f2ae5ad442e0e1a2a5
Syed Sayem <syed@sayem.org>
2015-06-21 03:32:03 -0400

    Rest API examples

---
+++ src/main/java/org/sayem/api/RestAdapter.java
@@ -0,0 +1,13 @@
+package org.sayem.api;
+
+import com.jayway.restassured.path.json.JsonPath;
+
+/**
+ * Created by syed.sayem on 6/21/15.
+ */
+
+public interface RestAdapter {
+
+    JsonPath execute();
+    <T> T execute(Class<T> responseClass);
+}
---
+++ src/test/java/org/sayem/api/Post.java
@@ -0,0 +1,40 @@
+package org.sayem.api;
+
+import com.jayway.restassured.path.json.JsonPath;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import java.io.IOException;
+
+public class Post {
+    private static TestDataProvider factory;
+
+    @Parameters("dataLocation")
+    public Post(@Optional(Repository.DATA_LOCATION)
+                             String dataLocation) throws IOException {
+        factory = TestDataProvider.create(dataLocation);
+    }
+
+    @Test
+    public void testPostWithPostMethod() {
+
+        JsonObject request = Json.createObjectBuilder()
+                .add("UserName", factory.data(Repository.USER_NAME))
+                .add("Password", factory.data(Repository.PASSWORD))
+                .build();
+
+        RestAdapter response = PostAdapter.builder()
+                .setContentType(ContentType.JSON)
+                .setRequestObject(request)
+                .setEndPoint(factory.data(Repository.ENDPOINT))
+                .setMethodName(factory.data(Repository.METHOD))
+                .build();
+
+        JsonPath jsonPath = response.execute();
+        jsonPath.prettyPrint();
+
+    }
+}
